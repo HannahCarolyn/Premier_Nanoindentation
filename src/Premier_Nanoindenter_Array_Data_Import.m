ad75f9fb5d24bdfe870dde96a73fe7da6fe21c66
Kieran Rivers <kieran-rivers@users.noreply.github.com>
2022-12-21 19:37:06 +0000

    Moved data input functions to source folder

---
+++ src/Premier_Nanoindenter_Array_Data_Import.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Premier_Nanoindenter_Array_Data_Import(inputArg1,inputArg2)
+%PREMIER_NANOINDENTER_ARRAY_DATA_IMPORT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ src/Premier_Nanoindenter_Mapping_Data_Import.m
@@ -0,0 +1,9 @@
+function [output1,output2] = Premier_Nanoindenter_Mapping_Data_Import(base_file_directory,xpm_pattern,rows,columns,bundle_spacing,row_overlap,column_overlap)
+    output1 = 2*x;
+    output2 = 3*x;
+end
+
+
+%need to convert serpentine to lateral
+%then convert lateral to full grid
+%then exclude overlap
