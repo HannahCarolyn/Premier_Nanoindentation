ea039a297bb332c8fe305852d933a56a51b4a9f3
Rebecca Tearle <118367909+ortearle@users.noreply.github.com>
2023-01-19 09:29:11 +0000

    Update dodgy_indent_find.m

--- src/dodgy_indent_find.m
+++ src/dodgy_indent_find.m
@@ -1,29 +1,27 @@
-function[dodgy_indent_plot]=dodgy_indent_find(struct, dodgy_indents,output_file_directory)
-% %I believe we need to change dodgy_indents to bad_indents_list and
-% %but don't need NaN info just this list
+function[dodgy_indent_plot]=dodgy_indent_find(struct, dodgy_indents,output_file_directory) %this function plots the location of the dodgy indents
 
 X_Coordinate='X_Coordinate';
 Y_Coordinate='Y_Coordinate';
 
-X=[struct.(X_Coordinate)].'; 
-Y=[struct.(Y_Coordinate)].';
+X=[struct.(X_Coordinate)].'; %x coordinates of indents
+Y=[struct.(Y_Coordinate)].'; %y coordinates of indents
 
-spacing=X(2,1)-X(1,1);
+spacing=X(2,1)-X(1,1); %spacing must be the same in x and y
 column_number=1+(max(X)/spacing); %this calculates the number of indents in x 
 row_number=1+(max(Y)/spacing); %this calculates the number of indents in y
-indent_number=column_number*row_number; %more of a sense check for me
+indent_number=column_number*row_number; %total indents
 
-X_append=1+(X./spacing); %assigns coordinates to array (row/column) position
-Y_append=1+(Y./spacing);
-grid_coord(:,1)=1:1:length(X); %to give indent number
+X_append=1+(X./spacing); %assigns x coordinates to array position (row)
+Y_append=1+(Y./spacing); %assigns y coordinates to array position (column)
+grid_coord(:,1)=1:1:length(X); %to give indent number, row and column number
 grid_coord(:,2)=X_append;
 grid_coord(:,3)=Y_append;
 
- dodgy=dodgy_indents.'; %had to transpose again
+ dodgy=dodgy_indents.'; %had to transpose to work
  coord_dodgy_x=zeros(length(dodgy),1);
  coord_dodgy_y=zeros(length(dodgy),1);
 
-for i=1:length(dodgy)
+for i=1:length(dodgy) %this for loop takes the indent number of the dodgy indents to find the corresponding array position. This is translated back to a coordinate.
     indent_number=dodgy(i,:);
     X_coord=grid_coord(indent_number,2);
     Y_coord=grid_coord(indent_number,3);
