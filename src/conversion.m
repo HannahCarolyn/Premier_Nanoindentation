aeba682c9af22b91bd9b761be5c9c448bcc5a273
Hannah <118382237+HannahCarolyn@users.noreply.github.com>
2023-02-13 11:22:55 +0000

    Updatingpremiermethodtobecomplete

--- src/conversion.m
+++ src/conversion.m
@@ -1,12 +1,12 @@
-function conversion(updated_data_struct,output_file_directory)
+function conversion(updated_main_data_struct,output_file_directory)
 
 %% Getting required info and setting up 3D arrays
 
-all_x_coordinates = [updated_data_struct.X_Coordinate]; % Gets all x coordinates
+all_x_coordinates = [updated_main_data_struct.X_Coordinate]; % Gets all x coordinates
 number_of_data = length(all_x_coordinates); % Gets number of indents
 unique_x_coordinates = unique(all_x_coordinates); % Create non-repeated list of all x coordinates in ascending order
 max_x_coordinate = max(all_x_coordinates); % Finds maximum x coordinate
-all_y_coordinates = [updated_data_struct.Y_Coordinate]; % Gets all y coordinates
+all_y_coordinates = [updated_main_data_struct.Y_Coordinate]; % Gets all y coordinates
 % Create non-repeated list of all y coordinates in ascending order
 max_y_coordinate = max(all_y_coordinates); % Finds maximum y coordinate
 coordinate_spacing = unique_x_coordinates(2) - unique_x_coordinates(1); % Calculates coordinate spacing from difference between two x coordinates
@@ -56,25 +56,25 @@ for x_coordinate_index = 1:length(possible_x_coordinates) % Loop through each po
     for y_coordinate_index = 1:length(possible_y_coordinates) % Loop through each possible y coordinate
         y_coordinate = possible_y_coordinates(y_coordinate_index); % Notes current y coordinate
         y_coordinate_find_indices = find(all_y_coordinates == y_coordinate,number_of_data); % Find indent indices corresponding to y coordinate
-        try % Try used in case gap in data in which case indent coordinate pair not exist 
+      try % Try used in case gap in data in which case indent coordinate pair not exist 
             indent_field_index = intersect(x_coordinate_find_indices,y_coordinate_find_indices); % Finds the specific indent of the current x,y coordinates based on struct field number (not indent number)
             % Writes values in correct order in fullres for CMM XPCorrelate
-            fullres(x_coordinate_index,y_coordinate_index,1) = updated_data_struct(indent_field_index).Surface_Displacement;
-            fullres(x_coordinate_index,y_coordinate_index,2) = updated_data_struct(indent_field_index).Maximum_Displacement;
-            fullres(x_coordinate_index,y_coordinate_index,3) = updated_data_struct(indent_field_index).Maximum_Load;
-            fullres(x_coordinate_index,y_coordinate_index,4) = updated_data_struct(indent_field_index).Youngs_Modulus;
-            fullres(x_coordinate_index,y_coordinate_index,5) = updated_data_struct(indent_field_index).Stiffness_Squared_Divided_By_Load;
-            fullres(x_coordinate_index,y_coordinate_index,6) = updated_data_struct(indent_field_index).Hardness;
+            fullres(x_coordinate_index,y_coordinate_index,1) = updated_main_data_struct(indent_field_index).Surface_Displacement;
+            fullres(x_coordinate_index,y_coordinate_index,2) = updated_main_data_struct(indent_field_index).Maximum_Displacement;
+            fullres(x_coordinate_index,y_coordinate_index,3) = updated_main_data_struct(indent_field_index).Maximum_Load;
+            fullres(x_coordinate_index,y_coordinate_index,4) = updated_main_data_struct(indent_field_index).Youngs_Modulus;
+            fullres(x_coordinate_index,y_coordinate_index,5) = updated_main_data_struct(indent_field_index).Stiffness_Sqaured_Divided_By_Load;
+            fullres(x_coordinate_index,y_coordinate_index,6) = updated_main_data_struct(indent_field_index).Hardness;
             % Writes values in fullres_additional for excel output (currently wrong orientation for output)
-            fullres_additional(x_coordinate_index,y_coordinate_index,1) = updated_data_struct(indent_field_index).Hardness;
-            fullres_additional(x_coordinate_index,y_coordinate_index,2) = updated_data_struct(indent_field_index).Youngs_Modulus;
-            fullres_additional(x_coordinate_index,y_coordinate_index,3) = updated_data_struct(indent_field_index).Reduced_Modulus;
-            fullres_additional(x_coordinate_index,y_coordinate_index,4) = updated_data_struct(indent_field_index).Stiffness;
-            fullres_additional(x_coordinate_index,y_coordinate_index,5) = updated_data_struct(indent_field_index).Maximum_Load;
-            fullres_additional(x_coordinate_index,y_coordinate_index,6) = updated_data_struct(indent_field_index).Maximum_Displacement;
-            fullres_additional(x_coordinate_index,y_coordinate_index,7) = updated_data_struct(indent_field_index).Surface_Displacement;
-            fullres_additional(x_coordinate_index,y_coordinate_index,8) = updated_data_struct(indent_field_index).Hardness_Divided_By_Modulus;
-            fullres_additional(x_coordinate_index,y_coordinate_index,9) = updated_data_struct(indent_field_index).Stiffness_Squared_Divided_By_Load;
+            fullres_additional(x_coordinate_index,y_coordinate_index,1) = updated_main_data_struct(indent_field_index).Hardness;
+            fullres_additional(x_coordinate_index,y_coordinate_index,2) = updated_main_data_struct(indent_field_index).Youngs_Modulus;
+            fullres_additional(x_coordinate_index,y_coordinate_index,3) = updated_main_data_struct(indent_field_index).Reduced_Modulus;
+            fullres_additional(x_coordinate_index,y_coordinate_index,4) = updated_main_data_struct(indent_field_index).Stiffness;
+            fullres_additional(x_coordinate_index,y_coordinate_index,5) = updated_main_data_struct(indent_field_index).Maximum_Load;
+            fullres_additional(x_coordinate_index,y_coordinate_index,6) = updated_main_data_struct(indent_field_index).Maximum_Displacement;
+            fullres_additional(x_coordinate_index,y_coordinate_index,7) = updated_main_data_struct(indent_field_index).Surface_Displacement;
+            fullres_additional(x_coordinate_index,y_coordinate_index,8) = updated_main_data_struct(indent_field_index).Hardness_Divided_By_Modulus;
+            fullres_additional(x_coordinate_index,y_coordinate_index,9) = updated_main_data_struct(indent_field_index).Stiffness_Sqaured_Divided_By_Load;
         catch % If coordinate pair not exist, NaN values written instead
             % Writes values in correct order in fullres for CMM XPCorrelate
             fullres(x_coordinate_index,y_coordinate_index,1) = NaN;
--- src/premier_method.m
+++ src/premier_method.m
@@ -1,4 +1,4 @@
-function [updated_data_struct] = premier_method(base_file_directory,updated_data_struct,mapping_type,naughty_indents_list,samplepoissonratio)
+function [updated_data_struct] = premier_method(base_file_directory,updated_main_data_struct,mapping_type,naughty_indents_list,samplepossionratio)
 
 % Values used in Young's Modulus calculation from tip area function file (copied from Hannah Oliver and Parr):
 area_function_path = strcat(base_file_directory,"Area_Function"); % Gets area function path
@@ -24,67 +24,74 @@ if mapping_type == "xpm_indentation_map"
             all_data(end+1,:) = data_input(data_row,:); % Append each row of data
         end
     end
-    for indent_loop = 1:length([updated_data_struct.Indent_Index]) % For count through number of indents in struct
-        if ismember(updated_data_struct(indent_loop).Indent_Index,naughty_indents_list) % Note naughty list always contains all error indents now
-            updated_data_struct(indent_loop).Maximum_Displacement = NaN; % NaN each value for this indent
-            updated_data_struct(indent_loop).Maximum_Load = NaN;
-            updated_data_struct(indent_loop).Surface_Displacement = NaN;
-            updated_data_struct(indent_loop).Hardness = NaN;
-            updated_data_struct(indent_loop).Reduced_Modulus = NaN;
-            updated_data_struct(indent_loop).Youngs_Modulus = NaN;
-            updated_data_struct(indent_loop).Stiffness = NaN;
-            updated_data_struct(indent_loop).Hardness_Divided_By_Modulus = NaN;
-            updated_data_struct(indent_loop).Stiffness_Squared_Divided_By_Load = NaN;
+    for indent_loop = 1:length([updated_main_data_struct.Indent_Index]) % For count through number of indents in struct
+        if ismember(updated_main_data_struct(indent_loop).Indent_Index,naughty_indents_list) % Note naughty list always contains all error indents now
+            updated_main_data_struct(indent_loop).Maximum_Displacement = NaN; % NaN each value for this indent
+            updated_main_data_struct(indent_loop).Maximum_Load = NaN;
+            updated_main_data_struct(indent_loop).Surface_Displacement = NaN;
+            updated_main_data_struct(indent_loop).Hardness = NaN;
+            updated_main_data_struct(indent_loop).Reduced_Modulus = NaN;
+            updated_main_data_struct(indent_loop).Youngs_Modulus = NaN;
+            updated_main_data_struct(indent_loop).Stiffness = NaN;
+            updated_main_data_struct(indent_loop).Hardness_Divided_By_Modulus = NaN;
+            updated_main_data_struct(indent_loop).Stiffness_Squared_Divided_By_Load = NaN;
         else
-            current_indent_index = updated_data_struct(indent_loop).Indent_Index; % Find index (starting at 0) of current indent
-            updated_data_struct(indent_loop).Maximum_Displacement = NaN % all_data(current_indent_index+1,?); % Find relevant values taking into account row index 1 more than indent index
-            updated_data_struct(indent_loop).Maximum_Load = all_data(current_indent_index+1,2);
-            updated_data_struct(indent_loop).Hardness = all_data(current_indent_index+1,8);
+            current_indent_index = updated_main_data_struct(indent_loop).Indent_Index; % Find index (starting at 0) of current indent
+            updated_main_data_struct(indent_loop).Maximum_Displacement = all_data(current_indent_index+1,5); % all_data(current_indent_index+1,?); % Find relevant values taking into account row index 1 more than indent index
+            updated_main_data_struct(indent_loop).Maximum_Load = all_data(current_indent_index+1,2);
+            updated_main_data_struct(indent_loop).Hardness = all_data(current_indent_index+1,8);
             Er = all_data(current_indent_index+1,7); % Er has seperate value as used to calculate Young's Modulus Below
-            updated_data_struct(indent_loop).Reduced_Modulus = Er; % Append Er
+            updated_main_data_struct(indent_loop).Reduced_Modulus = Er; % Append Er
             SampleEwithvin = 1/((1/Er) - (1-(tippoissonratio^2))/tipyoungmodulus); % Middle calculation step
-            E = (SampleEwithvin*(1-(samplepoissonratio^2))); %Young's modulus in (GPa) (copied from Hannah Oliver and Parr)
-            updated_data_struct(indent_loop).Youngs_Modulus = E; % Append E
-            updated_data_struct(indent_loop).Stiffness = all_data(current_indent_index+1,3); % Append stiffness
+            E = (SampleEwithvin*(1-(samplepossionratio^2))); %Young's modulus in (GPa) (copied from Hannah Oliver and Parr)
+            updated_main_data_struct(indent_loop).Youngs_Modulus = E; % Append E
+            updated_main_data_struct(indent_loop).Stiffness = all_data(current_indent_index+1,3); % Append stiffness
+            updated_data_struct=updated_main_data_struct;
         end
     end
 
 % Get data in case of grid array:
 else if mapping_type == "automated_indentation_grid_array"
-        premier_data_locations = strcat(base_file_directory,"Dynamic_Data"); % Gets the full folder path for the premier data (same file as coordinates)
+        premier_data_locations = strcat(base_file_directory,"Premier_Analysis"); % Gets the full folder path for the premier data (same file as coordinates)
         folder_info = dir(fullfile(premier_data_locations, '/*.txt')); % Gets a list of file properties within the folder
-        initial_number_of_data = size(folder_info,1); % Counts number of files in the folder
-        for file_loop = 1:initial_number_of_data % For count through number of indents in folder (note all indents are used so no need to bother with indent indices)
-            if ismember(updated_data_struct(file_loop).Indent_Index,naughty_indents_list) % Note naughty list always contains all error indents now
-                updated_data_struct(file_loop).Maximum_Displacement = NaN; % NaN each value for this indent
-                updated_data_struct(file_loop).Maximum_Load = NaN;
-                updated_data_struct(file_loop).Surface_Displacement = NaN;
-                updated_data_struct(file_loop).Hardness = NaN;
-                updated_data_struct(file_loop).Reduced_Modulus = NaN;
-                updated_data_struct(file_loop).Youngs_Modulus = NaN;
-                updated_data_struct(file_loop).Stiffness = NaN;
-                updated_data_struct(file_loop).Hardness_Divided_By_Modulus = NaN;
-                updated_data_struct(file_loop).Stiffness_Squared_Divided_By_Load = NaN;
+        noofindents= length([updated_main_data_struct.Indent_Index]);
+
+        for file_loop = 1:1:noofindents % For count through number of indents in folder (note all indents are used so no need to bother with indent indices)
+            if ismember(updated_main_data_struct(file_loop).Indent_Index,naughty_indents_list) % Note naughty list always contains all error indents now
+                updated_main_data_struct(file_loop).Maximum_Displacement = NaN; % NaN each value for this indent
+                updated_main_data_struct(file_loop).Maximum_Load = NaN;
+                updated_main_data_struct(file_loop).Surface_Displacement = NaN;
+                updated_main_data_struct(file_loop).Hardness = NaN;
+                updated_main_data_struct(file_loop).Reduced_Modulus = NaN;
+                updated_main_data_struct(file_loop).Youngs_Modulus = NaN;
+                updated_main_data_struct(file_loop).Stiffness = NaN;
+                updated_main_data_struct(file_loop).Hardness_Divided_By_Modulus = NaN;
+                updated_main_data_struct(file_loop).Stiffness_Squared_Divided_By_Load = NaN;
             else
-                file_name = folder_info(file_loop).name; % Extract file name of indent
-                full_file_name = fullfile(indent_file_locations, file_name); % Extract file name (including path) for each indent
+                file_name = folder_info.name;
+                full_file_name = fullfile(premier_data_locations, file_name); % Extract file name (including path) for each indent
                 full_input = importdata(full_file_name); % Extracts contents of file as struct depending on data structure
                 data_input = full_input.data; % Selects only the numerical data
-                % Maximum displacement
-
-                % Maximum load
-
-                % Surface displacement
-
-                % Hardness
-
-                % Moduli
-
-                % Stiffness
-
+                
+                
+                updated_main_data_struct(file_loop).Maximum_Displacement = data_input(file_loop,5); % NaN each value for this indent
+                updated_main_data_struct(file_loop).Maximum_Load = data_input(file_loop,2);
+                updated_main_data_struct(file_loop).Surface_Displacement = NaN;
+                updated_main_data_struct(file_loop).Hardness = data_input(file_loop,8);
+                Er = data_input(file_loop,7);
+                updated_main_data_struct(file_loop).Reduced_Modulus = data_input(file_loop,7);
+                SampleEwithvin = 1/((1/Er) - (1-(tippoissonratio^2))/tipyoungmodulus); % Middle calculation step
+                E = (SampleEwithvin*(1-(samplepossionratio^2))); %Young's modulus in (GPa) (copied from Hannah Oliver and Parr)
+                updated_main_data_struct(file_loop).Youngs_Modulus = E;
+                updated_main_data_struct(file_loop).Stiffness = data_input(file_loop,3);
+                updated_data_struct=updated_main_data_struct;
 
             end
         end
-    end
+end
+
+
+
+end
 end
         
